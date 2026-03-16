8ed50ebdbf4642dfe95fce1baa12bab36d022fc6
Jerry <66705911+Jerry-zh@users.noreply.github.com>
2026-03-16 20:37:12 +0800

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-fetch("https://baidu.com").then((data) => {
-    console.log(data);
-  });
-
-  setTimeout(() => {
-    fetch("https://baidu.com").then((data) => {
-      document.getElementById("root").textContent = data
-    });
-  }, 5000);
